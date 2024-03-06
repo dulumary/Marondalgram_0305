8917ba8797bc860ca75a6cd5a6e219e071095bdb
megait <megait@gmail.com>
2024-03-06 21:04:06 +0900

    [김인규]  - 프로젝트 생성

---
+++ src/main/java/com/marondal/marondalgram/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.marondal.marondalgram.hello;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HelloController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello World!!";
+	}
+
+}
